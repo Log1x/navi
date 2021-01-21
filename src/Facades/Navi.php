83014f58f369f252d6417ec6819ec7b3154ac8de
Brandon <brandon@tendency.me>
2021-01-21 12:59:35 -0600

    enhance(facade): Add phpdoc to facade

--- src/Facades/Navi.php
+++ src/Facades/Navi.php
@@ -4,6 +4,15 @@ namespace Log1x\Navi\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Log1x\Navi\Navi build(string $menu = 'primary_navigation')
+ * @method static mixed get(string $key = null, mixed $default = null)
+ * @method static bool isEmpty()
+ * @method static bool isNotEmpty()
+ *
+ * @see \Log1x\Navi\Navi
+ * @see \Illuminate\Support\Fluent
+ */
 class Navi extends Facade
 {
     /**
