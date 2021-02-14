eabb1a496bf45cc678851c633e5ee14cb8bd605f
Brandon <brandon@tendency.me>
2021-02-14 00:16:57 -0600

    chore(plugin): Use Acorn bootloader when registering as plugin with Sage chore(plugin): Register facade if using as plugin with Sage

--- plugin.php
+++ plugin.php
@@ -14,9 +14,15 @@ add_filter('after_setup_theme', function () {
         require_once $composer;
     }
 
-    if (function_exists('\Roots\app')) {
-        \Roots\app()->register(
-            Log1x\Navi\Providers\NaviServiceProvider::class
-        );
+    if (function_exists('Roots\bootloader')) {
+        Roots\bootloader(function (Roots\Acorn\Application $app) {
+            $app->register(
+                Log1x\Navi\Providers\NaviServiceProvider::class
+            );
+
+            Roots\Acorn\AliasLoader::getInstance([
+                'navi' => Log1x\Navi\Facades\Navi::class
+            ])->register();
+        });
     }
 }, 20);
