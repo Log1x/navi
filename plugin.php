74de35686654217865e19be750baf11bf1d41f68
Brandon <brandon@tendency.me>
2022-09-16 16:43:14 -0500

    v2.0.3

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name: Navi
  * Plugin URI:  https://github.com/log1x/navi
  * Description: A developer-friendly alternative to the WordPress NavWalker.
- * Version:     2.0.0
+ * Version:     2.0.3
  * Author:      Brandon Nifong
  * Author URI:  https://github.com/log1x
  */
