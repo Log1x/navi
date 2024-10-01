a58d90f08e31ccdfa36357ce6bc8a06fe950a274
Brandon <brandon@tendency.me>
2024-10-01 12:33:40 -0500

    🎨 Add missing methods to the Facade docblock

--- src/Facades/Navi.php
+++ src/Facades/Navi.php
@@ -5,10 +5,16 @@ namespace Log1x\Navi\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Log1x\Navi\Navi build(string $menu = 'primary_navigation')
+ * @method static \Log1x\Navi\Navi build(mixed $menu = null)
+ * @method static \Log1x\Navi\Navi withClasses(string|array $classes)
+ * @method static \Log1x\Navi\Navi withoutClasses(string|array $classes)
+ * @method static \Log1x\Navi\Navi withDefaultClasses()
  * @method static mixed get(string $key = null, mixed $default = null)
  * @method static bool isEmpty()
  * @method static bool isNotEmpty()
+ * @method static array all()
+ * @method static array toArray()
+ * @method static string toJson(int $options = 0)
  *
  * @see \Log1x\Navi\Navi
  */
